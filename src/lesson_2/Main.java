319c8a0ac2cd3ad874cf6684fd64ef89cacabf25
Mazura <dushacaterina@gmail.com>
2022-04-02 10:21:52 +0800

    Initial commit

---
+++ src/lesson_2/Main.java
@@ -0,0 +1,13 @@
+package lesson_2;
+
+public class Main {
+    public static void main(String[] args) {
+        String array[][] = {{"one", "two", "three", "four"}, {"apple", "pear", "banana", "pineapple"}};
+        try {
+            array[7][2] = ;
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("MyArraySizeException");
+
+                }
+            }
+        }
